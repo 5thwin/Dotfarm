9ce19af29a819f70344fc33af128980140596336
5thwin <hisy2059@naver.com>
2024-01-02 15:50:46 +0900

    FEAT: 스크롤에 따른 배경색 변경 추가

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,26 @@
+'use client'
 import { LandingHeaderButtonStyle } from '@/app/styles/common/buttonStyle';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { KakaoChatButton } from './KakaoChatButton';
-
+import { useEffect, useState } from 'react';
+import { throttle } from 'lodash'
 export default function Header() {
-  return <nav className={clsx('fixed top-0 left-0 right-0 p-5 z-30', 'flex items-center justify-between bg-white')}>
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = throttle(() => {
+      setIsScrolled(window.scrollY > 100);
+    }, 300);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      handleScroll.cancel();
+    };
+  }, []);
+  return <nav className={clsx('fixed top-0 left-0 right-0 p-5 z-30', 'flex items-center justify-between transition-colors', { 'bg-white': isScrolled, 'bg-transparent': !isScrolled })}>
     <Image
       src="/dotfarm.svg"
       alt="Dotfarm Logo"

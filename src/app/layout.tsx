060bf206a7d63542577a9b44a1edb5dc76ffdb96
5thwin <hisy2059@naver.com>
2024-01-02 17:47:04 +0900

    FEAT: 메타데이터 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,19 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'DotFarm',
   description: '한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드리며, 농업 경영 및 생산성 향상에 필요한 자료를 제공합니다.',
+
   openGraph: {
-    title: 'Blog',
+    title: 'DotFarm',
     description: '한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드리며, 농업 경영 및 생산성 향상에 필요한 자료를 제공합니다.',
   },
   icons: {
     icon: "/favicon.svg",
   },
+  verification: {
+    other: {
+      'naver-site-verification': `${process.env.NEXT_PUBLIC_NAVER_VERIFICATION}`,
+    }
+  }
 }
 
 export default function RootLayout({

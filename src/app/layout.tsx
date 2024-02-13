d6bc1269b543961611410c2f8dc9d868bf68fe59
5thwin <hisy2059@naver.com>
2024-02-13 12:57:13 +0900

    FIX: 타이틀 문구 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,12 @@ import Script from 'next/script';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: '농업 정보 플랫폼 ‘닷팜‘',
+	title: '농업 정보 플랫폼 닷팜',
 	description:
 		'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
 
 	openGraph: {
-		title: '농업 정보 플랫폼 ‘닷팜‘',
+		title: '농업 정보 플랫폼 닷팜',
 		description:
 			'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
 	},

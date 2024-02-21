925728791553cd7fa1eaddfa689f8f716520a292
5thwin <hisy2059@naver.com>
2024-02-21 13:18:42 +0900

    FIX: Layout 에서 meta 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,29 @@
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Head from 'next/head';
 import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-	title: '농업 정보 플랫폼 닷팜',
-	description:
-		'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
+// export const metadata: Metadata = {
+// 	title: '농업 정보 플랫폼 닷팜',
+// 	description:
+// 		'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
 
-	openGraph: {
-		title: '농업 정보 플랫폼 닷팜',
-		description:
-			'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
-	},
-	icons: {
-		icon: '/favicon.svg',
-	},
-	verification: {
-		other: {
-			'naver-site-verification': `${process.env.NEXT_PUBLIC_NAVER_VERIFICATION}`,
-		},
-	},
-};
+// 	openGraph: {
+// 		title: '농업 정보 플랫폼 닷팜',
+// 		description:
+// 			'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
+// 	},
+// 	icons: {
+// 		icon: '/favicon.svg',
+// 	},
+// 	verification: {
+// 		other: {
+// 			'naver-site-verification': `${process.env.NEXT_PUBLIC_NAVER_VERIFICATION}`,
+// 		},
+// 	},
+// };
 
 export default function RootLayout({
 	children,

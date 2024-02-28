c5dbf6fa753268629c85ae77225f09d377362663
5thwin <hisy2059@naver.com>
2024-02-28 14:46:52 +0900

    FIX: main css 수정

--- src/app/main/components/MainSideArea.tsx
+++ src/app/main/components/MainSideArea.tsx
@@ -5,7 +5,7 @@ export default function MainSideArea() {
 	return (
 		<aside
 			id="left-side-section"
-			className="flex flex-col gap-y-5 lg:gap-y-30px lg:w-[418px]"
+			className="flex flex-col gap-y-5 lg:gap-y-30px lg:min-w-[418px]"
 		>
 			<LoginBox />
 			<IssueBox />

c97e2614e8c96ee2e8a77b9815163bec8a1e1fd9
5thwin <hisy2059@naver.com>
2024-02-29 16:25:53 +0900

    FIX: favicon icon 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 			'한 곳에서 모든 농업 관련 지원 사업 및 정보를 쉽게 찾아보세요. 농부에게 필요한 최신 농업 정보와 지원 사업을 알려드립니다.',
 	},
 	icons: {
-		icon: '/favicon.svg',
+		icon: '/favicon.ico',
 	},
 	verification: {
 		other: {

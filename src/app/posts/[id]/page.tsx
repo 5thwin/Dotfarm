fca4f11c1bdbbc97ed42b9cc8394cdfe6381c73a
5thwin <hisy2059@naver.com>
2024-03-05 10:05:44 +0900

    FIX: generateStaticParams Error

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,6 +3,14 @@ import withLayout from '@/app/hoc/withLayout';
 import PostBox from './conponents/PostBox';
 import Fallback from './conponents/Fallback';
 
+export async function generateStaticParams() {
+	const posts = await getPosts();
+	if (!posts) return [];
+	return posts.map((post) => ({
+		id: post.id.toString(),
+	}));
+}
+
 async function Page({ params }: { params: { id: string } }) {
 	const { id } = params;
 	const post = await getPost(Number(id));
@@ -13,13 +21,6 @@ async function Page({ params }: { params: { id: string } }) {
 			</section>
 		</div>
 	);
-}
-export async function generateStaticParams() {
-	const posts = await getPosts();
-
-	return posts?.map((post) => ({
-		id: post.id.toString(),
-	}));
 }
 
 export default withLayout(Page, true, false);

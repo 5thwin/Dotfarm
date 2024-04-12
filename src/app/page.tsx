706a5d893336112c6e8b89fa01721f63f8ae0d87
5thwin <hisy2059@naver.com>
2024-04-12 11:11:15 +0900

    FIX: remove checking code about env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,9 +31,6 @@ export const metadata: Metadata = {
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">
-			<p>
-				env: {CLIENT_ID} {REDIRECT_URI}
-			</p>
 			<Header />
 			<HeroSection />
 			<ProblemSection />

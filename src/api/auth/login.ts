96a3765a2944211df690d293526d6bf5815ae677
5thwin <hisy2059@naver.com>
2024-04-12 17:56:06 +0900

    FIX: login error handler

--- src/api/auth/login.ts
+++ src/api/auth/login.ts
@@ -17,25 +17,22 @@ export const login = async (code: string) => {
 	// Basic 인증을 위한 인코딩된 문자열 생성
 	const params = new URLSearchParams({ code });
 	const queryString = params.toString();
-	try {
-		const res = await customFetch<LoginResponse>(
-			`/auth/oauth/callback?${queryString}`,
-			{
-				method: 'GET',
-			}
-		);
-		const { accessToken, refreshToken } = res;
-		// 쿠키에 accessToken과 refreshToken을 저장합니다.
-		// 쿠키 옵션은 필요에 따라 설정할 수 있습니다.
 
-		setAccessTokenInCookie(accessToken);
+	const res = await customFetch<LoginResponse>(
+		`/auth/oauth/callback?${queryString}`,
+		{
+			method: 'GET',
+		}
+	);
+	const { accessToken, refreshToken } = res;
+	// 쿠키에 accessToken과 refreshToken을 저장합니다.
+	// 쿠키 옵션은 필요에 따라 설정할 수 있습니다.
 
-		const getAccess = getAccessTokenFromCookie();
-		setRefreshTokenInCookie(refreshToken);
-		return res;
-	} catch (e) {
-		console.error(e);
-	}
+	setAccessTokenInCookie(accessToken);
+
+	const getAccess = getAccessTokenFromCookie();
+	setRefreshTokenInCookie(refreshToken);
+	return res;
 };
 
 export const logout = async () => {

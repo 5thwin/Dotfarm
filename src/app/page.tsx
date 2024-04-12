eb249ad68bc6089e321f39bf0ce1940d14bc835a
5thwin <hisy2059@naver.com>
2024-04-12 11:07:53 +0900

    FIX: check env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">
 			<p>
-				{CLIENT_ID} {REDIRECT_URI}
+				env: {CLIENT_ID} {REDIRECT_URI}
 			</p>
 			<Header />
 			<HeroSection />

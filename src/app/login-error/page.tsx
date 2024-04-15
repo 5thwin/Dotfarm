13069453cf6ca48a3759b8d8d2113a56e0e9e18e
5thwin <hisy2059@naver.com>
2024-04-15 10:20:27 +0900

    FIX: Link

--- src/app/login-error/page.tsx
+++ src/app/login-error/page.tsx
@@ -1,16 +1,8 @@
-'use client';
 import React from 'react';
 import { KaKaoLoginButton } from '../components/landing/KaKaoLoginButton';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const LoginErrorPage: React.FC = () => {
-	const router = useRouter();
-
-	// 메인 페이지로 돌아가는 함수
-	const handleGoToMain = () => {
-		router.push('/');
-	};
-
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
 			<div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full text-center">
@@ -21,12 +13,12 @@ const LoginErrorPage: React.FC = () => {
 					로그인 과정에서 문제가 발생했습니다. 잠시 후, 다시 시도해 주세요.
 				</p>
 				<KaKaoLoginButton />
-				<button
-					className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-					onClick={handleGoToMain}
+				<Link
+					href={'/main'}
+					className="mt-4 bg-mainGreen hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
 				>
 					메인으로 돌아가기
-				</button>
+				</Link>
 			</div>
 		</div>
 	);

c1591591bd14150f14711a9aeada1f0e6c22333a
5thwin <hisy2059@naver.com>
2024-04-15 10:22:53 +0900

    FIX: login error page fix

--- src/app/login-error/page.tsx
+++ src/app/login-error/page.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { KaKaoLoginButton } from '../components/landing/KaKaoLoginButton';
 import Link from 'next/link';
+import { KakaoBubble } from '../components/icons/KakaoBubble';
 
 const LoginErrorPage: React.FC = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-			<div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full text-center">
+			<div className="bg-white shadow-lg rounded-lg p-8 max-w-md w-full text-center flex flex-col items-center">
 				<h1 className="text-xl font-bold text-gray-800 mb-4">
 					로그인 오류 발생
 				</h1>
-				<p className="text-gray-600 mb-6">
-					로그인 과정에서 문제가 발생했습니다. 잠시 후, 다시 시도해 주세요.
+				<p className="text-gray-600 mb-6 text-center">
+					로그인 과정에서 문제가 발생했습니다.
+					<br /> 잠시 후, 다시 시도해 주세요.
 				</p>
-				<KaKaoLoginButton />
+				<KaKaoLoginButton className="bg-kakaoYellow rounded-xl text-black font-bold flexCenter py-3 px-6">
+					<div className="flex gap-x-1">
+						<KakaoBubble />
+						<span className="text-opacity-85">카카오 로그인</span>
+					</div>
+				</KaKaoLoginButton>
 				<Link
 					href={'/main'}
 					className="mt-4 bg-mainGreen hover:bg-green-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"

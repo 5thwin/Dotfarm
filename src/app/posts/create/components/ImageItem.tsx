f6e4a6022cb058ea9435396e12f33fadde0eb667
5thwin <hisy2059@naver.com>
2024-04-18 17:15:42 +0900

    FIX: check error contents in error handler

--- src/app/posts/create/components/ImageItem.tsx
+++ src/app/posts/create/components/ImageItem.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import useCreateImageStore from '../store/createImageStore';
 import clsx from 'clsx';
+import { getFullImagePath } from '@/utils/image';
 
 type Props = {
 	imageUrl: string;
@@ -14,7 +15,7 @@ export default function ImageItem({ imageUrl, index }: Props) {
 	};
 	return (
 		<div className="relative rounded-10 w-[110px] lg:w-[185px] h-full overflow-hidden">
-			<Image alt="게시글 이미지" fill src={imageUrl}></Image>
+			<Image alt="게시글 이미지" fill src={getFullImagePath(imageUrl)}></Image>
 			<button className={deleteStyle} onClick={handleDelete}>
 				제거
 			</button>

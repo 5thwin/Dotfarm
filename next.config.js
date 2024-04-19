b3e3039d3876796ecc0cdb07a4ea8bff1f3ca569
5thwin <hisy2059@naver.com>
2024-04-19 18:02:18 +0900

    FIX: image upOptimized

--- next.config.js
+++ next.config.js
@@ -4,17 +4,13 @@ const path = require("path");
 const isStaticBuild = process.env.OUTPUT_BUILD_OPTION === 'export'
 const nextConfig = {
   ...(isStaticBuild ? { output: 'export' } : {}), // 수정된 부분
+  images: { unoptimized: true },
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
   },
   webpack: (config) => {
     config.module.rules.push({
@@ -23,6 +19,7 @@ const nextConfig = {
     });
     return config;
   },
+
 };
 
 module.exports = nextConfig;

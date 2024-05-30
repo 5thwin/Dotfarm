b03201b0f53d55c8242c1a2f516375eb930bc738
5thwin <hisy2059@naver.com>
2024-05-30 10:09:10 +0900

    FIX: Merge error

---
+++ src/app/support-projects/components/CalendarDateProgramItem.tsx
@@ -0,0 +1,44 @@
+import { SupportProgram } from '@/type/support';
+import { calculateDday } from '@/utils/date/compare';
+import { getRecruitmentStatus } from '@/utils/supportPrograms';
+import { Tooltip } from 'react-tooltip';
+import clsx from 'clsx';
+
+type Props = { program: SupportProgram };
+
+export default function CalendarDateProgramItem({ program }: Props) {
+	const recruitmentStatus = getRecruitmentStatus(program);
+	return (
+		<li className={listStyle}>
+			<div>
+				{recruitmentStatus === 'IS_RECRUITING' && (
+					<span className="text-mainGreen font-bold text-sm whitespace-nowrap">
+						{`D-${calculateDday(program.deadline)}`}
+					</span>
+				)}
+				{recruitmentStatus === 'IS_CLOSED' && (
+					<span className="text-warnRed font-bold text-sm whitespace-nowrap">
+						마감
+					</span>
+				)}
+				{recruitmentStatus === 'IS_ALWAYS' && (
+					<span className="text-subText font-bold text-sm whitespace-nowrap">
+						상시
+					</span>
+				)}
+			</div>
+			<Tooltip id="my-tooltip" />
+			<p
+				data-tooltip-id="my-tooltip"
+				data-tooltip-content={program.programName}
+				className={clsx('text-sm line-clamp-2', {
+					'text-lineColor': recruitmentStatus === 'IS_CLOSED',
+				})}
+			>
+				{program.programName}
+			</p>
+		</li>
+	);
+}
+// style
+const listStyle = clsx('flex gap-x-2.5 items-center');

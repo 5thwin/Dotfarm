a8ca9d5cf51f54bc08f42df30065cbee34127753
5thwin <hisy2059@naver.com>
2024-05-31 15:37:20 +0900

    FIX: Render fallback components when there is no data to display in the watchlist

--- src/app/main/components/banner/WatchList.tsx
+++ src/app/main/components/banner/WatchList.tsx
@@ -1,6 +1,7 @@
 import { getMyInterests } from '@/api/user/interest/get';
 import { getMyLikes } from '@/api/user/likes/get';
 import WatchItem from './WatchItem';
+import NotResultFallback from '@/app/components/fallback/NotResultFallback';
 
 type MyWatchList = {
 	category: string;
@@ -31,6 +32,16 @@ export default async function WatchList() {
 		(a, b) => new Date(b.createAt).getTime() - new Date(a.createAt).getTime()
 	); // Sorting by createAt in descending order
 
+	if (myWatchLists.length === 0) {
+		return (
+			<NotResultFallback className="flexCenter flex-col gap-y-2.5 h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+				<p className="text-subText font-bold text-center">
+					좋아요를 누른 <br />
+					게시글이 없습니다.
+				</p>
+			</NotResultFallback>
+		);
+	}
 	return (
 		<ul className="flex flex-col gap-y-[18px] overflow-auto">
 			{myWatchLists.map((watchlist, index) => (

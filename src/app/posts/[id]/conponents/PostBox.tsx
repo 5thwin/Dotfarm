f6d36c57203fd7753e553e4b02eb370aa39f8769
5thwin <hisy2059@naver.com>
2024-06-26 10:40:40 +0900

    FIX:  an issue with <a> tag styles not being applied inside posts

--- src/app/posts/[id]/conponents/PostBox.tsx
+++ src/app/posts/[id]/conponents/PostBox.tsx
@@ -10,7 +10,6 @@ import { Post } from '@/type/post';
 import { getUserIdByAccessToken } from '@/api/auth/token/utils';
 import { getFullImagePath } from '@/utils/image';
 import Link from 'next/link';
-import { linkify } from '@/utils/string';
 import PreviousPostsBox from './PreviousPostsBox';
 
 type Props = {
@@ -65,3 +64,42 @@ const containerStyle = clsx(
 	'flex flex-col gap-y-5 lg:gap-y-30px p-25px'
 );
 const postWrapper = clsx('flex flex-col gap-y-2.5');
+
+// 이 위치에 있어야 <a> 태그가 정상적으로 변환됩니다.
+// 주어진 텍스트에서 URL을 찾아 링크로 변환하는 함수
+export function linkify(text: string): React.ReactNode {
+	// URL을 찾기 위한 정규 표현식
+	const urlRegex =
+		/(\bhttps?:\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+	// 적절한 형태로 매치되고 남은 부분을 배열로 나누기
+	const parts: React.ReactNode[] = [];
+	let lastOffset = 0;
+
+	// replace 함수를 사용하여 URL과 그 주변 텍스트를 처리
+	text.replace(urlRegex, (match, p1, offset) => {
+		// URL 앞의 텍스트를 추가
+		parts.push(text.slice(lastOffset, offset));
+		// URL을 <a> 태그로 변환하여 추가
+		parts.push(
+			<a
+				key={offset}
+				href={match}
+				target="_blank"
+				rel="noopener noreferrer"
+				className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+			>
+				{match}
+			</a>
+		);
+		// 마지막 처리 위치를 업데이트
+		lastOffset = offset + match.length;
+		return match;
+	});
+
+	// 마지막 URL 이후의 텍스트를 추가
+	if (lastOffset < text.length) {
+		parts.push(text.slice(lastOffset));
+	}
+
+	return parts;
+}
